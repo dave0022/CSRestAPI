2056542eb74638a64915a22bbf503833f6c0a738
dave0022 <deiviss50@gmail.com>
2017-09-17 17:00:58 +0200

    Updates

--- CustomerAppBLL/BusinessObjects/CustomerBO.cs
+++ CustomerAppBLL/BusinessObjects/CustomerBO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CustomerAppBLL.BusinessObjects
@@ -8,6 +9,7 @@ namespace CustomerAppBLL.BusinessObjects
     {
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
