2a0ca81e909f6b63869d01aa9d67a70378a29706
dave0022 <deiviss50@gmail.com>
2017-09-17 19:45:09 +0200

    BLL Order and DAL UOW update

---
+++ CustomerAppBLL/Converters/OrderConverter.cs
@@ -0,0 +1,33 @@
+﻿using CustomerAppBLL.BusinessObjects;
+using CustomerAppDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAppBLL.Converters
+{
+    class OrderConverter
+    {
+        internal Order Convert(OrderBO order)
+        {
+            return new Order()
+            {
+                Id = order.Id,
+                DeliveryDate = order.DeliveryDate,
+                OrderDate = order.OrderDate
+              
+            };
+        }
+
+        internal OrderBO Convert(Order order)
+        {
+            return new OrderBO()
+            {
+                Id = order.Id,
+                DeliveryDate = order.DeliveryDate,
+                OrderDate = order.OrderDate
+               
+            };
+        }
+    }
+}
---
+++ CustomerAppDAL/UOW/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CustomerAppDAL.Context;
+using CustomerAppDAL.Repositories;
+
+namespace CustomerAppDAL.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICustomerRepository CustomerRepository { get; internal set; }
+
+        public IOrderRepository OrderRepository { get; internal set; }
+
+        private CustomerAppContext context;
+
+        public UnitOfWork()
+        {
+            context = new CustomerAppContext();
+            CustomerRepository = new CustomerRepositoryEFMemory(context);
+            OrderRepository = new OrderRepository(context);
+        }
+
+		public int Complete()
+		{
+			//The number of objects written to the underlying database.
+			return context.SaveChanges();
+		}
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+    }
+}
