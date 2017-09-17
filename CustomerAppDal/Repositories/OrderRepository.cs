cf47baf4cd41f7694f8ec1e62241c7b78b538ed6
dave0022 <deiviss50@gmail.com>
2017-09-17 18:19:00 +0200

    Another Dal Update

---
+++ CustomerAppDal/Repositories/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerAppDAL.Entities;
+using CustomerAppDAL.Context;
+using System.Linq;
+
+namespace CustomerAppDAL.Repositories
+{
+    class OrderRepository : IOrderRepository
+    {
+        CustomerAppContext _context;
+        public OrderRepository(CustomerAppContext context)
+        {
+            _context = context;
+        }
+        public Order Create(Order order)
+        {
+
+            _context.Orders.Add(order);
+            return order;
+        }
+
+        public Order Delete(int Id)
+        {
+            var order = Get(Id);
+            _context.Orders.Remove(order);
+            return order;
+        }
+
+        public Order Get(int Id)
+        {
+            return _context.Orders.FirstOrDefault(o => o.Id == Id);
+        }
+
+        public List<Order> GetAll()
+        {
+            return _context.Orders.ToList();
+        }
+    }
+}
